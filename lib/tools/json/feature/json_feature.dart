82efb6604019b725d4ec2714325f0c029f91fc83
Vorkytaka <vorkytaka@gmail.com>
2024-12-01 00:51:16 +0300

    #82: Make initial effects unmodifiable

--- lib/tools/json/feature/json_feature.dart
+++ lib/tools/json/feature/json_feature.dart
@@ -4,17 +4,12 @@ import 'dart:isolate';
 import 'package:flutter/cupertino.dart';
 import 'package:json_path/json_path.dart';
 
-import '../../../common/mini_tea/effect_handler/debounce_effect_handler.dart';
 import '../../../common/mini_tea/feature/feature.dart';
 
 part 'json_effect.dart';
-
 part 'json_effect_handler.dart';
-
 part 'json_event.dart';
-
 part 'json_state.dart';
-
 part 'json_update.dart';
 
 typedef JsonFeature = Feature<JsonState, JsonEvent, JsonEffect, void>;
