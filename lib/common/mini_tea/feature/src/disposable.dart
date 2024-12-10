ad2a06bb5c831ed5939f6b69fa3dd0f7daeaa5f9
Vorkytaka <vorkytaka@gmail.com>
2024-12-11 00:41:27 +0300

    –: Make Effect Handler interface only

--- lib/common/mini_tea/feature/src/disposable.dart
+++ lib/common/mini_tea/feature/src/disposable.dart
@@ -1,6 +1,6 @@
 part of 'feature.dart';
 
-/// Can be used with class that implements [IEffectHandler].
+/// Can be used with class that implements [EffectHandler].
 /// If so, then feature will called dispose for this effect handler.
 // ignore:one_member_abstracts
 abstract interface class Disposable {
