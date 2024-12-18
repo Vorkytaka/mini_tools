59b830bb2d507ae73e8b833067975c9064fbf144
Vorkytaka <vorkytaka@gmail.com>
2024-12-19 01:34:17 +0300

    Base refactor

--- lib/tools/material_colors_tool.dart
+++ lib/tools/material_colors_tool.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:macos_ui/macos_ui.dart';
+import 'package:responsive_grid/responsive_grid.dart';
 
 import '../common/color.dart';
 import '../i18n/strings.g.dart';
@@ -12,30 +13,124 @@ final materialColorTool = BaseTool(
   screenBuilder: (context) => const MaterialColorTool(),
 );
 
-const _materialColors = <MaterialColor>[
-  Colors.red,
-  Colors.pink,
-  Colors.purple,
-  Colors.deepPurple,
-  Colors.indigo,
-  Colors.blue,
-  Colors.lightBlue,
-  Colors.cyan,
-  Colors.teal,
-  Colors.green,
-  Colors.lightGreen,
-  Colors.lime,
-  Colors.yellow,
-  Colors.amber,
-  Colors.orange,
-  Colors.deepOrange,
-  Colors.brown,
-  Colors.grey,
-  Colors.blueGrey,
+class _MaterialAndAccentColor {
+  final String title;
+  final MaterialColor color;
+  final MaterialAccentColor? accentColor;
+
+  const _MaterialAndAccentColor(
+    this.title,
+    this.color, [
+    this.accentColor,
+  ]);
+}
+
+const _materialAndAccentColors = [
+  _MaterialAndAccentColor(
+    'Red',
+    Colors.red,
+    Colors.redAccent,
+  ),
+  _MaterialAndAccentColor(
+    'Pink',
+    Colors.pink,
+    Colors.pinkAccent,
+  ),
+  _MaterialAndAccentColor(
+    'Purple',
+    Colors.purple,
+    Colors.purpleAccent,
+  ),
+  _MaterialAndAccentColor(
+    'Deep Purple',
+    Colors.deepPurple,
+    Colors.deepPurpleAccent,
+  ),
+  _MaterialAndAccentColor(
+    'Indigo',
+    Colors.indigo,
+    Colors.indigoAccent,
+  ),
+  _MaterialAndAccentColor(
+    'Blue',
+    Colors.blue,
+    Colors.blueAccent,
+  ),
+  _MaterialAndAccentColor(
+    'Light Blue',
+    Colors.lightBlue,
+    Colors.lightBlueAccent,
+  ),
+  _MaterialAndAccentColor(
+    'Cyan',
+    Colors.cyan,
+    Colors.cyanAccent,
+  ),
+  _MaterialAndAccentColor(
+    'Teal',
+    Colors.teal,
+    Colors.tealAccent,
+  ),
+  _MaterialAndAccentColor(
+    'Green',
+    Colors.green,
+    Colors.greenAccent,
+  ),
+  _MaterialAndAccentColor(
+    'Light Green',
+    Colors.lightGreen,
+    Colors.lightGreenAccent,
+  ),
+  _MaterialAndAccentColor(
+    'Lime',
+    Colors.lime,
+    Colors.limeAccent,
+  ),
+  _MaterialAndAccentColor(
+    'Yellow',
+    Colors.yellow,
+    Colors.yellowAccent,
+  ),
+  _MaterialAndAccentColor(
+    'Amber',
+    Colors.amber,
+    Colors.amberAccent,
+  ),
+  _MaterialAndAccentColor(
+    'Orange',
+    Colors.orange,
+    Colors.orangeAccent,
+  ),
+  _MaterialAndAccentColor(
+    'Deep Orange',
+    Colors.deepOrange,
+    Colors.deepOrangeAccent,
+  ),
+  _MaterialAndAccentColor(
+    'Brown',
+    Colors.brown,
+  ),
+  _MaterialAndAccentColor(
+    'Grey',
+    Colors.grey,
+  ),
+  _MaterialAndAccentColor(
+    'Blue Grey',
+    Colors.blueGrey,
+  ),
 ];
 
-final _materialColorItems =
-    _materialColors.map((color) => color.items).toList(growable: false);
+final _materialColorItems = _materialAndAccentColors
+    .map(
+      (item) => _ColorCollection(
+        name: item.title,
+        colors: [
+          ...item.color.items,
+          ...?item.accentColor?.items,
+        ],
+      ),
+    )
+    .toList(growable: false);
 
 class MaterialColorTool extends StatelessWidget {
   const MaterialColorTool({super.key});
@@ -67,78 +162,68 @@ class _Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO(Vorkytaka): Use CustomScrollView
-    return SingleChildScrollView(
-      controller: controller,
-      child: Wrap(
-        spacing: 10,
-        runSpacing: 10,
-        alignment: WrapAlignment.start,
-        children: _materialColorItems
-            .map((colors) => _MaterialColor(colors: colors))
-            .toList(growable: false),
-      ),
+    return ResponsiveGridList(
+      desiredItemWidth: 200,
+      minSpacing: 10,
+      children: _materialColorItems
+          .map((colors) => _MaterialColor(collection: colors))
+          .toList(growable: false),
     );
   }
 }
 
 class _MaterialColor extends StatelessWidget {
-  final List<_ColorItem> colors;
+  final _ColorCollection collection;
 
   const _MaterialColor({
-    required this.colors,
+    required this.collection,
   });
 
   @override
   Widget build(BuildContext context) {
+    final colors = collection.colors;
+
     return Column(
-      children: colors
-          .map(
-            (color) => MouseRegion(
-              cursor: SystemMouseCursors.click,
-              child: GestureDetector(
-                onTap: () {
-                  Clipboard.setData(ClipboardData(text: color.hex));
-                },
-                child: Container(
-                  width: 200,
-                  height: 50,
-                  color: color.color,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 12),
-                    child: DefaultTextStyle.merge(
-                      style: TextStyle(
-                        color: color.brightness == Brightness.dark
-                            ? Colors.white
-                            : Colors.black,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [Text(color.title), Text(color.hex)],
-                      ),
+      children: [
+        for (int i = 0; i < colors.length; i++)
+          MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: GestureDetector(
+              onTap: () {
+                Clipboard.setData(ClipboardData(text: colors[i].hex));
+              },
+              child: Container(
+                width: 200,
+                height: 50,
+                color: colors[i].color,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  child: DefaultTextStyle.merge(
+                    style: TextStyle(
+                      color: colors[i].brightness == Brightness.dark
+                          ? Colors.white
+                          : Colors.black,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(i == 0
+                            ? '${collection.name} ${colors[i].title}'
+                            : colors[i].title),
+                        Text(colors[i].hex)
+                      ],
                     ),
                   ),
                 ),
               ),
             ),
           )
-          .toList(growable: false),
+      ],
     );
   }
 }
 
-const _materialColorsValues = [
-  50,
-  100,
-  200,
-  300,
-  400,
-  500,
-  600,
-  700,
-  800,
-  900,
-];
+const _materialColorsValues = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
 
 extension on MaterialColor {
   List<_ColorItem> get items {
@@ -157,6 +242,37 @@ extension on MaterialColor {
       growable: false,
     );
   }
+}
+
+const _materialAccentColorsValues = [100, 200, 400, 700];
+
+extension on MaterialAccentColor {
+  List<_ColorItem> get items {
+    return List.generate(
+      _materialAccentColorsValues.length,
+      (i) {
+        final shade = _materialAccentColorsValues[i];
+        final color = this[shade]!;
+        return _ColorItem(
+          color: color,
+          title: '$shade',
+          hex: color.toHexString,
+          brightness: ThemeData.estimateBrightnessForColor(color),
+        );
+      },
+      growable: false,
+    );
+  }
+}
+
+class _ColorCollection {
+  final String name;
+  final List<_ColorItem> colors;
+
+  const _ColorCollection({
+    required this.name,
+    required this.colors,
+  });
 }
 
 @immutable
