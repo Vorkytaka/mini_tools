58f03ef584332a459c35129922d688e9eaa69574
Vorkytaka <vorkytaka@gmail.com>
2025-02-16 22:07:56 +0300

    #154: Fixes

--- lib/tools/datetime_converter/datetime_converter_tool.dart
+++ lib/tools/datetime_converter/datetime_converter_tool.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:macos_ui/macos_ui.dart';
 import 'package:mini_tea_flutter/mini_tea_flutter.dart';
-import 'package:timezone/timezone.dart';
 
 import '../../i18n/strings.g.dart';
 import '../../tool/feature_tool.dart';
 import 'datetime_converter_screen.dart';
-import 'datetime_cubit.dart';
 import 'feature/datetime_converter_feature.dart';
 
 final datetimeTool = FeatureTool(
@@ -16,13 +13,6 @@ final datetimeTool = FeatureTool(
   screenBuilder: (context) => const DatetimeConverterScreen(),
   feature: datetimeConverterFeatureFactory()..init(),
 );
-
-Widget buildDatetimeTool(TZDateTime datetime) {
-  return BlocProvider(
-    create: (context) => DatetimeCubit(initialDateTime: datetime),
-    child: const DatetimeConverterScreen(),
-  );
-}
 
 Future<void> showDatetimeConverterSheet({
   required BuildContext context,
