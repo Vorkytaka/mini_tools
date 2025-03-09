a2480157e975887868c3453450b1adb489292ad3
Vorkytaka <vorkytaka@gmail.com>
2025-03-09 22:12:38 +0300

    #193: Base for `HintButton`

---
+++ lib/common/ui/hint_button.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_popup/flutter_popup.dart';
+import 'package:macos_ui/macos_ui.dart';
+import 'package:macos_ui/src/utils/utils.dart';
+
+class HintButton extends StatelessWidget {
+  final Widget hint;
+
+  HintButton({
+    required String hint,
+    super.key,
+  }) : hint = Text(hint);
+
+  const HintButton.rich({
+    required this.hint,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = MacosTheme.of(context);
+
+    return CustomPopup(
+      arrowColor: theme.popupButtonTheme.popupColor,
+      backgroundColor: theme.popupButtonTheme.popupColor,
+      content: ConstrainedBox(
+        constraints: const BoxConstraints(
+          maxWidth: 300,
+        ),
+        child: hint,
+      ),
+      child: CircleAvatar(
+        backgroundColor: theme.helpButtonTheme.color,
+        foregroundColor: helpIconLuminance(
+          theme.helpButtonTheme.color!,
+          theme.brightness == Brightness.dark,
+        ),
+        radius: 10,
+        child: const Icon(
+          CupertinoIcons.question,
+          size: 13,
+        ),
+      ),
+    );
+  }
+}
