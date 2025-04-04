14c69cfe4a28b7022bdd6509d2d56b1e81b7f2da
vorkytaka <vorkytaka@gmail.com>
2025-04-05 01:27:53 +0300

    #45: Use notification on material color tool

--- lib/tools/material_color/material_color_ui.dart
+++ lib/tools/material_color/material_color_ui.dart
@@ -87,6 +87,7 @@ class _ColorItemWidget extends StatelessWidget {
       child: GestureDetector(
         onTap: () {
           Clipboard.setData(ClipboardData(text: item.hex));
+          CopyOverlay.show(context);
         },
         child: Container(
           width: _itemWidth,
